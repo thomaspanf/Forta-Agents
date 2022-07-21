1e717a5f801471aae0e041fa9bf9f454525a8750
Thomas Pan <thomaspanf@gmail.com>
2022-07-20 19:39:32 -0700

    feat: init proj

---
+++ PancakeSwap-Bots/Set-Masterchef-Settings/src/abi.ts
@@ -0,0 +1,10 @@
+const CAKE: string[] = [
+    "function setMigrator(IMigratorChef _migrator)",
+    "function dev(address _devaddr)",
+    "function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) ",
+    "function updateMultiplier(uint256 multiplierNumber)",
+  ];
+  
+  export default {
+    CAKE,
+  };
