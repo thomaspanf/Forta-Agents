0b3bf912e3e2aecd4edb3ae4d7586a2d7e5dc000
Thomas Pan <thomaspanf@gmail.com>
2022-08-01 10:39:49 -0700

    feat: trying to implement networkmanager

--- PancakeSwap-Bots/Set-Masterchef-Settings/src/agent.ts
+++ PancakeSwap-Bots/Set-Masterchef-Settings/src/agent.ts
@@ -1,15 +1,33 @@
 import { Finding, HandleTransaction, TransactionEvent, getEthersProvider } from "forta-agent";
 import { providers } from "ethers";
-import NetworkData from "./network";
-import NetworkManager from "./network";
+// import NetworkData from "./network";
+// import NetworkManager from "./network";
 import utils from "./utils";
 import abi from "./abi";
+import { NetworkManager } from "forta-agent-tools";
 
-const networkManager = new NetworkManager();
+interface NetworkData {
+  masterChef: string;
+  num: number; 
+}
+
+const data: Record<number, NetworkData> = {
+  56:{
+    masterChef: "0x73feaa1eE314F8c655E354234017bE2193C9E24E",
+    num: 1,
+  },
+  97:{
+    masterChef: "0xbD315DA028B586f7cD93903498e671fA3efeF506", 
+    num: 2, 
+  },
+};
+
+const networkManager = new NetworkManager(data);
 
 export const initialize = (provider: providers.Provider) => async () => {
-  const { chainId } = await provider.getNetwork();
-  networkManager.setNetwork(chainId);
+  await networkManager.init(provider);  
+  // const { chainId } = await provider.getNetwork();
+  // networkManager.setNetwork(chainId);
 };
 
 export const handleTransaction =
--- PancakeSwap-Bots/Set-Masterchef-Settings/src/network.ts
+++ PancakeSwap-Bots/Set-Masterchef-Settings/src/network.ts
@@ -1,31 +1,31 @@
 
-interface NetworkData {
-  masterChef: string;
-  num: number; 
-}
+// interface NetworkData {
+//   masterChef: string;
+//   num: number; 
+// }
 
-const data: Record<number, NetworkData> = {
-  56:{
-    masterChef: "0x73feaa1eE314F8c655E354234017bE2193C9E24E",
-    num: 1,
-  },
-  97:{
-    masterChef: "0xbD315DA028B586f7cD93903498e671fA3efeF506", 
-    num: 2, 
-  },
-};
+// const data: Record<number, NetworkData> = {
+//   56:{
+//     masterChef: "0x73feaa1eE314F8c655E354234017bE2193C9E24E",
+//     num: 1,
+//   },
+//   97:{
+//     masterChef: "0xbD315DA028B586f7cD93903498e671fA3efeF506", 
+//     num: 2, 
+//   },
+// };
 
-export default class NetworkManager implements NetworkData {
-  public masterChef: string;
-  public num: number; 
+// export default class NetworkManager implements NetworkData {
+//   public masterChef: string;
+//   public num: number; 
 
-  constructor() {
-    this.masterChef = "";
-    this.num = 0; 
-  }
+//   constructor() {
+//     this.masterChef = "";
+//     this.num = 0; 
+//   }
 
-  public setNetwork(networkId: number) {
-    const { masterChef } = data[networkId];
-    this.masterChef = masterChef;
-  }
-}
+//   public setNetwork(networkId: number) {
+//     const { masterChef } = data[networkId];
+//     this.masterChef = masterChef;
+//   }
+// }
