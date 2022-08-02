e7d42b21bc858d7aa63d593d2eb30d7aaaa2e25e
Thomas Pan <thomaspanf@gmail.com>
2022-08-02 12:37:37 -0700

    feat: implementing gam networkmanager

--- PancakeSwap-Bots/Set-Masterchef-Settings/src/network.ts
+++ PancakeSwap-Bots/Set-Masterchef-Settings/src/network.ts
@@ -1,30 +1,47 @@
-interface NetworkData {
+// interface NetworkData {
+//   masterChef: string;
+// }
+
+type AgentConfig = Record<number, NetworkData>;
+
+const CONFIG: AgentConfig = {
+  56:{
+    masterChef: "0x73feaa1eE314F8c655E354234017bE2193C9E24E",
+  },
+  97:{
+    masterChef: "0xbD315DA028B586f7cD93903498e671fA3efeF506", 
+  },
+};
+
+export interface NetworkData {
   masterChef: string;
 }
+export default CONFIG;
 
-const BSC_MAINNET_DATA: NetworkData = {
-  masterChef: "0x73feaa1eE314F8c655E354234017bE2193C9E24E",
-};
 
-//PoC
-const BSC_TESTNET_DATA: NetworkData = {
-  masterChef: "0xbD315DA028B586f7cD93903498e671fA3efeF506",
-};
+// const BSC_MAINNET_DATA: NetworkData = {
+//   masterChef: "0x73feaa1eE314F8c655E354234017bE2193C9E24E",
+// };
 
-const NETWORK_MAP: Record<number, NetworkData> = {
-  56: BSC_MAINNET_DATA,
-  97: BSC_TESTNET_DATA,
-};
+// //PoC
+// const BSC_TESTNET_DATA: NetworkData = {
+//   masterChef: "0xbD315DA028B586f7cD93903498e671fA3efeF506",
+// };
 
-export default class NetworkManager implements NetworkData {
-  public masterChef: string;
+// const NETWORK_MAP: Record<number, NetworkData> = {
+//   56: BSC_MAINNET_DATA,
+//   97: BSC_TESTNET_DATA,
+// };
 
-  constructor() {
-    this.masterChef = "";
-  }
+// export default class NetworkManager implements NetworkData {
+//   public masterChef: string;
 
-  public setNetwork(networkId: number) {
-    const { masterChef } = NETWORK_MAP[networkId];
-    this.masterChef = masterChef;
-  }
-}
+//   constructor() {
+//     this.masterChef = "";
+//   }
+
+//   public setNetwork(networkId: number) {
+//     const { masterChef } = NETWORK_MAP[networkId];
+//     this.masterChef = masterChef;
+//   }
+// }
